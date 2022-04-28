010bb8b6ae9e96049b6d309374656db8d0ca26f2
neerajnair72 <69789124+neerajnair72@users.noreply.github.com>
2022-04-28 16:34:03 +0530

    Update single.conf.js

--- conf/single.conf.js
+++ conf/single.conf.js
@@ -1,9 +1,10 @@
 
-username= process.env.LT_USERNAME || "<your username>",
-accessKey=  process.env.LT_ACCESS_KEY || "<your accessKey>",
+username= 'neerajn',
+accessKey=  'qJWmY51AY1vdlTC04Kh4iPttDsTgVl330z39YG4FadfqebaTSo',
 
 exports.config = {
   'specs': ['../specs/single.js'],
+  'specs': ['../specs/parallel.js'],
 
   seleniumAddress: 'https://'+ username +':'+ accessKey  +'@hub.lambdatest.com/wd/hub',
 
